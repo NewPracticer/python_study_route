68e9306ea01b0a3dfea6b8b6be645540029812ab
tianqing.liang <1>
2022-02-22 14:37:58 +0800

    '布尔及字符串'

--- data.py
+++ data.py
@@ -1,13 +1,47 @@
 if __name__ == "__main__" :
-    # int
+    ## int
     print(type(1))
     print(type(-1))
-    # float
+    ## float
     print(type(1.1))
     print(type(1.1111))
     print(type(1+1.0))
-    # *
+    ## *
     print(type(1 *1.0))
-    # /
+    ## /
     print(type(1 / 1 ))
     print(type(1 // 1 ))
+    ## 二进制 八进制 十六进制
+    print(0b10);
+    print(0o10);
+    print(0x10);
+    ## 进制转化
+    print(bin(10));
+    print(int(0b1010));
+    print(hex(0b1010));
+    ##bool布尔类型 真或假
+    print(True);
+    print(False);
+    print(type(True));
+    print(type(False));
+    print(int(True));
+    print(int(False));
+    print(bool(1));
+    print(bool(1.1));
+    print(bool(0));
+    print(bool('abc'));
+    print(bool(''));
+    print(bool([1,2,3]));
+    print(bool([]));
+    print(bool(None));
+    ## 字符串
+    print('hello world')
+    print("hello world")
+    print(type('1'))
+    print("let's go");
+    print('let\'s go');
+    print('''
+     hello world
+     hello world
+     hello world
+     ''');
