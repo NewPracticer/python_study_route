2b96f71e618116656a5c5966c935dd7fbc0b5665
tianqing.liang <1>
2022-02-24 16:13:51 +0800

    '序列集合字典'

--- data.py
+++ data.py
@@ -34,7 +34,7 @@ if __name__ == "__main__" :
     print(bool([1,2,3]));
     print(bool([]));
     print(bool(None));
-    ## 字符串
+    # 字符串
     print('hello world')
     print("hello world")
     print(type('1'))
@@ -45,7 +45,7 @@ if __name__ == "__main__" :
      hello world
      hello world
      ''');
-    ## 字符串运算
+    # 字符串运算
     print("hello"+"world")
     print("hello"*3)
     print("hello world"[0])
@@ -65,8 +65,8 @@ if __name__ == "__main__" :
     print('c:\\northwind\\northwest')
     print(r'c:\northwind\northwest')
     print(R'c:\northwind\northwest')
-    ## print(r'let's go'); 错误写法
-    ## 列表
+    # print(r'let's go'); 错误写法
+    # 列表
     print(type([1,2,3,4,5,6]))
     print(type(["hello,world",1,2,3,4,5,6]))
     print(type(["hello,world",1,2,3,4,5,6,True,False]))
@@ -75,11 +75,50 @@ if __name__ == "__main__" :
     print(["hello,world",1,2,3,4,5,6,True,False][0:2])
     print(["hello,world",1,2,3,4,5,6,True,False][-1:])
     print(["hello,world",1,2,3,4,5,6,True,False]+ ["点燃","虚弱"])
-    ## 元组
+    # 元组
     print((1,2,3,4,5))
     print((1,2,3,4,5)[0])
     print((1,2,3,4,5)[0:2])
     print((1,2,3,4,5)* 3)
     print(type((1,2,3,4,5)))
     print(type((1)))
+    print(type((1,)))
+    print(type(()))
+    print(type([1]))
+    # str list tuple 序列
+    print("hello world"[2])
+    print([1,2,3][2])
+    # 切片
+    print([1,2,3,4,5][0:3])
+    print([1,2,3,4,5][-1:])
+    print("hello world"[0:8:2])
+    print(3 in [1,2,3,4,5,6])
+    print(3 not in [1,2,3,4,5,6])
+    print(len([1,2,3,4,5,6]))
+    print(max([1,2,3,4,5,10]))
+    print(min([1,2,3,4,5,10]))
+    print(min([0,1,2,3,4,5,10]))
+    print(min("abcde"))
+    print(ord('w'))
+    # Set 无序集合,没有重复元素
+    print(type({1,1,2,2,3,4,5,6}))
+    print(len({1,1,2,2,3,4,5,6}))
+    print(1 in {1,1,2,2,3,4,5,6})
+    print(1 not in {1,1,2,2,3,4,5,6})
+    # 求两个集合的差集
+    print({1,1,2,2,3,4,5,6} - {3,4})
+    # 求两个集合的交集
+    print({1, 1, 2, 2, 3, 4, 5, 6} & {3, 4})
+    # 求两个集合的合集
+    print({1, 1, 2, 2, 3, 4, 5, 6} | {3, 4,7})
+    print(type({}))
+    print(type(set()))
+    # dict 字典
+    # {key1:value1,key2:value2....}
+    print(type({1:1,2:2,3:3}))
+    print({"Q":"新月打击","W":"苍白之瀑","E":"月之降临","R":"月神冲刺"})
+    print({"Q":"新月打击","W":"苍白之瀑","E":"月之降临","R":"月神冲刺"}["Q"])
+    print({"Q":"新月打击","W":"苍白之瀑","E":"月之降临","R":"月神冲刺","Q":"test新月打击"}["Q"])
+    print({"Q":"新月打击","W":"苍白之瀑","E":"月之降临","R":"月神冲刺","Q":"test新月打击",1:"新月打击"})
+
 
