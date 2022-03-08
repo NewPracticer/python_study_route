a9d46577e37d501b25897133af2455cc41f435d5
tianqing.liang <1>
2022-03-08 15:55:23 +0800

    函数

---
+++ fourth/c15.py
@@ -0,0 +1,54 @@
+# 类的基本作用：封装
+# 类是现实世界或思维世界中的实体在计算机中的反映
+# 它将数据以及这些数据上的操作封装在一起
+# 类 - > 类变量
+# 实例 - > 实例变量
+class Student():
+
+    name = 'skyl'
+    age = 0
+    sum = 0
+    # 实例方法
+    def __init__(self,name,age):
+        # 构造函数
+        # 初始化对象的属性
+        self.name = name
+        self.age = age
+        # print(age)
+        # print(name)
+        self.__class__.sum +=1
+        print('当前班级学生总数为：'+str(self.__class__.sum))
+
+    def print_file(self):
+        print('name: '+self.name)
+        print('age:  '+str(self.age))
+
+    #类方法
+    @classmethod
+    def plus_sum(cls):
+        cls.sum +=1 
+        print(cls.sum)
+
+
+
+# 类的实例化
+student = Student('xiao',18)
+# student2 = Student('da',118)
+Student.plus_sum()
+student.print_file()
+# student2.print_file()
+# print(Student.name)
+print(student.__dict__)
+
+student2 = Student('zhong', 19)
+Student.plus_sum()
+student3 = Student('da', 20)
+Student.plus_sum()
+
+
+
+
+
+
+
+        
